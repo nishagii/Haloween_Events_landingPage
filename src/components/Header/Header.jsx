e709b8da6122a20e9d370141a6f8737be61f9947
nishagii <jeewanthadeherath04@gmail.com>
2024-12-31 09:03:07 +0530

    Add Header component with styling and content for Halloween Festival

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+          <div className="header-contents">
+        <h1>Boooooo-tipul Helloween Bash </h1>
+        <p className='para'>Welcome to the Helloween Festival! Dive into the spooky spirit of Halloween with our thrilling events, haunting decorations. Prepare to be scared and amazed as we celebrate the most haunted holiday of the year!</p>
+              <button>View Items</button>
+          </div>
+    </div>
+  )
+}
+
+export default Header
